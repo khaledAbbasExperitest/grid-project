a027092ccb58d5ff8688b3e0a5c6872821d06757
khaledAbbasExperitest <khaled.abbas@experitest.com>
2017-09-24 14:17:01 +0300

    FirstClass

--- src/main/java/framework/Runner.java
+++ src/main/java/framework/Runner.java
@@ -1,5 +1,8 @@
+package framework;
+
 /**
  * Created by khaled.abbas on 9/24/2017.
  */
 public class Runner {
+
 }
---
+++ src/main/java/framework/SeeTestExtendedClient.java
@@ -0,0 +1,7 @@
+package framework;
+
+/**
+ * Created by khaled.abbas on 9/24/2017.
+ */
+public class SeeTestExtendedClient {
+}
